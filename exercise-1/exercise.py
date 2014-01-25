002bd128870ec3f957884ced699bb16584701dc2
Derek Chiang <derekchiang93@gmail.com>
2014-01-25 23:12:56 +0000

    Use keystone client

--- exercise-1/exercise.py
+++ exercise-1/exercise.py
@@ -1,5 +1,8 @@
+import os
+
 from glanceclient import Client as GlanceClient
 from novaclient.v1_1.client import Client as NovaClient
+from keystoneclient.v2_0.client import Client as KeystoneClient
 import simplejson as json
 
 
@@ -9,14 +12,19 @@ def read_config(filename):
 
 if __name__ == '__main__':
     config = read_config("config.json")
+    keystone = KeystoneClient(username=config['username'],
+                              password=config['password'],
+                              auth_url=config['authUrl'],
+                              tenant_name=config['tenantName'])
     glance = GlanceClient('1', endpoint=config[
-        'glancePublicUrl'], token=config['authToken'])
+        'glancePublicUrl'], token=keystone.auth_token)
     nova = NovaClient(config['username'], config['password'],
                       config['tenantName'], auth_url=config['authUrl'])
 
+    # Use the first flavor by default
     fl = nova.flavors.list()[0]
     for image in glance.images.list():
-        if 'ubuntu' in getattr(image, 'name'):
-            nova.servers.create(name=getattr(image, 'name'),
-                                image=getattr(image, 'id'), flavor=fl)
+        if 'ubuntu' in image.name:
+            nova.servers.create(name=image.name,
+                                image=image.id, flavor=fl)
 
